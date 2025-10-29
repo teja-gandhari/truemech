7d4e1a3c7235bbd0e384f2c795e24c7c3f681f89
dharma7095-den <gandhari7095@gmail.com>
2025-10-29 23:17:18 +0530

    commit

--- lib/features/auth/screens/auth_screen/login_form.dart
+++ lib/features/auth/screens/auth_screen/login_form.dart
@@ -13,59 +13,60 @@ class LoginPage extends StatelessWidget {
     final TextEditingController passwordController = TextEditingController();
     final _formKey = GlobalKey<FormState>();
 
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 32.h),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Email",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                ),
-                const SizedBox(height: 4),
-                CustomTextField(
-                  controller: emailController,
-                  nameHint: 'name@example.com',
-                  prefixIcon: Icons.mail,
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                Row(
-                  children: <Widget>[
-                    const Text(
-                      "Password",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-                    ),
-                    const Spacer(),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text('Forgot?'),
-                    ),
-                  ],
-                ),
-                CustomTextField(
-                  controller: passwordController,
-                  nameHint: 'Enter your password',
-                  prefixIcon: Icons.lock,
-                  obscureText: true,
-                  keyboardType: TextInputType.text,
-                ),
-                const SizedBox(height: 12),
-                CustomeButton(
-                  onPressed: () {},
-                  borderRadius: 18.0,
-                  child: const Text('Login',style: TextStyle(fontWeight: FontWeight.w500,fontSize: 18,color:Colors.white ),),
-                ),
-                const SizedBox(height: 14,),
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 32.h),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Email",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                  ),
+                  const SizedBox(height: 4),
+                  CustomTextField(
+                    controller: emailController,
+                    nameHint: 'name@example.com',
+                    prefixIcon: Icons.mail,
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      const Text(
+                        "Password",
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                      ),
+                      const Spacer(),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('Forgot?'),
+                      ),
+                    ],
+                  ),
+                  CustomTextField(
+                    controller: passwordController,
+                    nameHint: 'Enter your password',
+                    prefixIcon: Icons.lock,
+                    obscureText: true,
+                    keyboardType: TextInputType.text,
+                  ),
+                  const SizedBox(height: 12),
+                  CustomeButton(
+                    onPressed: () {},
+                    borderRadius: 18.0,
+                    child: const Text('Login',style: TextStyle(fontWeight: FontWeight.w500,fontSize: 18,color:Colors.white ),),
+                  ),
+                  const SizedBox(height: 14,),
 
-              ],
+                ],
+              ),
             ),
           ),
         ),
-      ),
     );
   }
 }
--- lib/features/auth/screens/auth_screen/login_screen.dart
+++ lib/features/auth/screens/auth_screen/login_screen.dart
@@ -17,54 +17,56 @@ class _LoginAuthState extends State<LoginAuth> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: SizedBox(
-            width: ScreenUtil().screenWidth,
-            child: Column(
-              children: [
-                ClipRect(
-                  child: Image.asset('',height:100 ,width:100 ,fit: BoxFit.cover,),
-                ),
-                const SizedBox(height: 5,),
-                //add fonts
-                Text('True Mech',style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),),
-                const SizedBox(height: 5,),
-                Text('All in one vehicle service',style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),),
-
-                const SizedBox(height: 8,),
-                AnimatedToggleSwitch<bool>.size(current: loginPage,
-                    values: const [false , true],
-                  iconOpacity: 0.2,
-                  indicatorSize: Size.fromWidth(220),height: 40,
-                   customIconBuilder: (context,local,global)=>
-                  Text(local.value ? 'Sign up' : 'Login',style: TextStyle(fontSize: 18,color: Color.lerp(Colors.black, Colors.black, local.animationValue)),
-                  ),borderWidth: 2.0,
-                  iconAnimationType: AnimationType.onHover,
-                  style: ToggleStyle(
-                    indicatorColor: Color.fromARGB(199, 72,47,247),
-                    borderColor: Colors.transparent,
-                    borderRadius: BorderRadius.circular(24),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withValues(),
-                        blurRadius: 2
-                      )
-                    ],
+        child: SingleChildScrollView(
+          child: Center(
+            child: SizedBox(
+              width: ScreenUtil().screenWidth,
+              child: Column(
+                children: [
+                  ClipRect(
+                    child: Image.asset('',height:100 ,width:100 ,fit: BoxFit.cover,),
                   ),
-                  selectedIconScale: 1.0,
-                  onChanged: (value)=> setState(()=>loginPage=value),
-                ),
-                SizedBox(
-                  height: 400,
-                  child: AnimatedSwitcher(duration:const Duration(milliseconds: 500)
-                  ,
-                  transitionBuilder: (child,animation)=>FadeTransition(opacity: animation,child: child)
-                    ,child:loginPage
-                          ? const SingUpScreen(key: ValueKey('siqnup'))
-                          : const LoginPage(key: ValueKey('login'))),
-                ),
-        
-              ],
+                  const SizedBox(height: 5,),
+                  //add fonts
+                  Text('True Mech',style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),),
+                  const SizedBox(height: 5,),
+                  Text('All in one vehicle service',style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),),
+          
+                  const SizedBox(height: 8,),
+                  AnimatedToggleSwitch<bool>.size(current: loginPage,
+                      values: const [false , true],
+                    iconOpacity: 0.2,
+                    indicatorSize: Size.fromWidth(220),height: 40,
+                     customIconBuilder: (context,local,global)=>
+                    Text(local.value ? 'Sign up' : 'Login',style: TextStyle(fontSize: 18,color: Color.lerp(Colors.black, Colors.black, local.animationValue)),
+                    ),borderWidth: 2.0,
+                    iconAnimationType: AnimationType.onHover,
+                    style: ToggleStyle(
+                      indicatorColor: Color.fromARGB(199, 72,47,247),
+                      borderColor: Colors.transparent,
+                      borderRadius: BorderRadius.circular(24),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          blurRadius: 2
+                        )
+                      ],
+                    ),
+                    selectedIconScale: 1.0,
+                    onChanged: (value)=> setState(()=>loginPage=value),
+                  ),
+                  SizedBox(
+                    height: 400,
+                    child: AnimatedSwitcher(duration:const Duration(milliseconds: 500)
+                    ,
+                    transitionBuilder: (child,animation)=>FadeTransition(opacity: animation,child: child)
+                      ,child:loginPage
+                            ? const SingUpScreen(key: ValueKey('siqnup'))
+                            : const LoginPage(key: ValueKey('login'))),
+                  ),
+          
+                ],
+              ),
             ),
           ),
         ),
