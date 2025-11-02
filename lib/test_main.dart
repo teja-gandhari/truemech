a6c4ce0fce09acfa91208853e83f23fd133c775f
dharma7095-den <gandhari7095@gmail.com>
2025-11-02 19:38:54 +0530

    commit

---
+++ lib/test_main.dart
@@ -0,0 +1,21 @@
+import  'package:flutter/material.dart';
+
+import 'features/onboarding/screens/onboarding_screen.dart';
+
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'True Mech',
+      home: OnboardingScreen(),
+          );
+  }
+}
